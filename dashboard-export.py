700734f66a9ebac2a6ccbad4d3a122d0297c1e91
Filip Balák <fbalak@redhat.com>
2018-07-03 08:18:38 -0400

    add dashboard-export.py

---
+++ dashboard-export.py
@@ -0,0 +1,23 @@
+import requests
+
+# link to server with grafana api
+api_url = ""
+
+# list of dashboards to export
+dashboards = ["brick", "cluster", "host", "volume"]
+data = {key: [] for key in dashboards}
+
+for d in dashboards:
+    r = requests.get('{}:3000/api/dashboards/db/{}-dashboard'.format(
+        api_rul, d))
+    rows = r.json()["dashboard"]["rows"]
+    for row in rows:
+        data[d].append({row["title"]:[]})
+        for panel in row["panels"]:
+            if panel["title"]:
+                print("{},{},{}".format(d,row["title"],panel["title"]))
+                data[d][-1][row["title"]].append(panel["title"])
+            elif "displayName" in panel.keys() and panel["displayName"]:
+                print("{},{},{}".format(d,row["title"],panel["displayName"]))
+                data[d][-1][row["title"]].append(panel["displayName"])
+return data
